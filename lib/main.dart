18ced28349c8bc58f7e23638e27fa97c25121374
kimsj1993 <kimsj1993@gmail.com>
2020-01-29 23:19:29 -0600

    creating new, custom widget, Stateless widget rerendered when input data changes while Stateful widget is rerendered when inputdata or local state changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 /* void main(){
   runApp(MyApp());
 } */
@@ -34,7 +36,7 @@ class _MyAppState extends State<MyApp> {
           ),
           body: Column(
             children: <Widget>[
-              Text(questions[_questionIndex]),
+              Question(questions[_questionIndex]),
               RaisedButton(
                 child: Text('Answer 1'),
                 onPressed: _answerQuestion,
